833027e08104a030ce55a93656c4da14c5cca646
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-29 03:06:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(str)
 str_array = str.split
 str_array.map do |char|
 r_array = []
-char.reverse_each { |c| r_array << c }
+c = char.split(//)
+c.reverse_each { |x| r_array << x }
 r_sentence = r_array.join(" ")
 end
 end
