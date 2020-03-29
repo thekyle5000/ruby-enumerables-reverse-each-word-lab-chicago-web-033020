03cac0bf35c7c8eea51b282d72d746331d009782
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-29 03:28:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,13 @@
 require 'pry'
 def reverse_each_word(str) 
 str_array = str.split
-
+final_array = []
 str_array.map do |char|
 r_array = []
 c = char.split(//)
 binding.pry
 c.reverse_each { |x| r_array << x }
-r_sentence = r_array.join(" ")
+final_array << r_array.join()
+r_sentence = final_array.join(" ")
 end
 end 
